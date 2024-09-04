425851eca5f596690b672d24e63eb744186cd941
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-04 23:55:25 +0100

    ff

--- index.php
+++ index.php
@@ -24,8 +24,23 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num01 = $_POST["num01"];
+        $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+        $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST["operator"]);
+
+        // Error trackers
+        $errors = false;
+
+        if (empty($num01)  || empty($num02) || empty($operator)) {
+            echo "<p class='error'>Fill all the fields!</p>";
+            $errors = true;
+        }
+        if (!is_numeric($num01) || !is_numeric($num02)) {
+            echo "<p class='error'>Write numbers only!</p>";
+            $errors = true;
+        }
     }
+
     ?>
 </body>
 
