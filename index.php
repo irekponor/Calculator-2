00a15446369d363a056e76e352dad7f43237da06
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-04 23:02:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <input type="number" name="num01" placeholder="number 1"> <br>
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num02" placeholder="number 2">
+        <button>Calculate</button>
+    </form>
+
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    }
+    ?>
+</body>
+
+</html>
